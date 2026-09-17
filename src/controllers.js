eb5c729ec7acc01e1c6c125155b39c7a3a629b8d
LarissaGuarizo <larissaguarizotolloto@gmail.com>
2026-09-17 11:29:32 -0300

    inventario

--- src/controllers.js
+++ src/controllers.js
@@ -1,17 +1,22 @@
 const bens = require("../bens.json")
 
-function autoIncrement(){
+function autoIncrement() {
     const ultimoId = Number(bens[bens.length - 1].id)
     return ultimoId + 1;
 }
 //CRUDS
-const create = (req, res)=>{
-    const dados = read.body
-    bens.add(dados)
-    res.status(201).json(dados)
+const create = (req, res) => {
+    const dados = req.body
+    bens.push(dados)
+    //res.status(201).json(dados)
+    res.redirect("http://localhost:5500")
 }
 
 
-const read = (req, res)=>{
+const read = (req, res) => {
     res.json(bens)
+}
+
+module.exports = {
+    create, read
 }
